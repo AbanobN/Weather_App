17313506da1ab477b17d8a7398abfcb0d259b82d
AbanobN <abanob.nabil.nagib@gmail.com>
2024-09-19 01:14:10 +0300

    init project structure

---
+++ app/src/main/java/com/example/weatherapplication/ui/alerts/AlertsFragment.kt
@@ -0,0 +1,40 @@
+package com.example.weatherapplication.ui.alerts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapplication.databinding.FragmentAlertsBinding
+
+class AlertsFragment : Fragment() {
+
+    private var _binding: FragmentAlertsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val alertsViewModel =
+            ViewModelProvider(this).get(AlertsViewModel::class.java)
+
+        _binding = FragmentAlertsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textAlerts
+        alertsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/weatherapplication/ui/settings/SettingsFragment.kt
@@ -0,0 +1,40 @@
+package com.example.weatherapplication.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapplication.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSettings
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
